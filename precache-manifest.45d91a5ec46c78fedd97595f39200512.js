ba915da37590a19d5ac0525b050d46a3580b0445
张安南 <Annan@ResNet-29-81.resnet.ucsb.edu>
2020-04-29 20:17:43 -0700

    Updates

---
+++ precache-manifest.45d91a5ec46c78fedd97595f39200512.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6252f4c81c610646fcd8be77e7ab1045",
+    "url": "/cs185-react/index.html"
+  },
+  {
+    "revision": "2d4ed048944abdb6d7f0",
+    "url": "/cs185-react/static/css/main.b29b320d.chunk.css"
+  },
+  {
+    "revision": "8d442352d290a72c6992",
+    "url": "/cs185-react/static/js/2.e03abf32.chunk.js"
+  },
+  {
+    "revision": "4e6d57ea575a16b77026958ba0efff35",
+    "url": "/cs185-react/static/js/2.e03abf32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4ed048944abdb6d7f0",
+    "url": "/cs185-react/static/js/main.e14a7b9f.chunk.js"
+  },
+  {
+    "revision": "4fb2594c8a0afa2fa1d8",
+    "url": "/cs185-react/static/js/runtime-main.f225555f.js"
+  },
+  {
+    "revision": "7c02e48e980b8c2b6e5f76c246df420e",
+    "url": "/cs185-react/static/media/00.7c02e48e.jpeg"
+  },
+  {
+    "revision": "b35693c3183c35feb0ddba7513186f1c",
+    "url": "/cs185-react/static/media/01.b35693c3.jpeg"
+  },
+  {
+    "revision": "adbe10889e085d1fb40a74c281fdbba2",
+    "url": "/cs185-react/static/media/02.adbe1088.jpeg"
+  },
+  {
+    "revision": "30d5347c32382ea5d519369f242af0db",
+    "url": "/cs185-react/static/media/03.30d5347c.jpeg"
+  },
+  {
+    "revision": "af2f839f0be0176d0b0e1eebf5b48c7c",
+    "url": "/cs185-react/static/media/04.af2f839f.jpeg"
+  },
+  {
+    "revision": "6ea110142d546c216cd0ff683876b06c",
+    "url": "/cs185-react/static/media/05.6ea11014.jpeg"
+  },
+  {
+    "revision": "6f4aaf8a1adbe8f73ee6df63755ec73e",
+    "url": "/cs185-react/static/media/06.6f4aaf8a.jpeg"
+  },
+  {
+    "revision": "f8a5060c882fc947e78830b969998bcd",
+    "url": "/cs185-react/static/media/07.f8a5060c.jpeg"
+  },
+  {
+    "revision": "9a6cf162b3169014eff7827c9708051c",
+    "url": "/cs185-react/static/media/csgo.9a6cf162.mp4"
+  },
+  {
+    "revision": "433e469b0880af3c5776d57874f2d19f",
+    "url": "/cs185-react/static/media/me.433e469b.jpeg"
+  },
+  {
+    "revision": "8647a2e0777c7d0978855e2263828674",
+    "url": "/cs185-react/static/media/patent.8647a2e0.jpeg"
+  }
+]);
