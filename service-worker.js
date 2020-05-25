c3b7b4988001bccfd399a8f50120f396f0d921ab
张安南 <Annan@ResNet-29-81.resnet.ucsb.edu>
2020-05-25 01:44:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-react/precache-manifest.9717864d0c5d3675f73252292c8f80e4.js"
+  "/cs185-react/precache-manifest.4ba841a05ef6970cf28639cda1be205d.js"
 );
 
 self.addEventListener('message', (event) => {
